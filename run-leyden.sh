9c0be5f03c05db2f20b8e21e6d27ef52a4a5e7b0
Aleksey Shipilev <aleksey@shipilev.net>
2024-08-22 17:41:00 +0200

    Re-run

--- run-leyden.sh
+++ run-leyden.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 
-# Config
-#OPTS="-Xmx256m -Xms256m -XX:+UseSerialGC"
-#OPTS="-Xmx256m -Xms256m -XX:+UseParallelGC"
-OPTS="-Xmx256m -Xms256m -XX:+UseG1GC"
-#OPTS="-Xmx256m -Xms256m -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"
+sudo cpupower frequency-set -g performance
 
+# Config
 HF_OPTS="-w 50 -r 100"
 
 # Pull the binaries if not present
@@ -17,6 +14,11 @@ fi
 if [ ! -d jdk-21 ]; then
   curl https://builds.shipilev.net/openjdk-jdk21-dev/openjdk-jdk21-dev-linux-x86_64-server.tar.xz | tar xJf -
   mv jdk/ jdk-21/
+fi
+
+if [ ! -d jdk-23 ]; then
+  curl https://builds.shipilev.net/openjdk-jdk23/openjdk-jdk23-linux-x86_64-server.tar.xz | tar xJf -
+  mv jdk/ jdk-23/
 fi
 
 if [ ! -d jdk-mainline ]; then
@@ -31,6 +33,7 @@ fi
 
 J17=jdk-17
 J21=jdk-21
+J23=jdk-23
 JM=jdk-mainline
 JL=jdk-leyden
 
@@ -50,37 +53,52 @@ public class HelloStream {
 }
 EOF
 
-OPTS="$OPTS -cp hellostream.jar"
-rm -f *.aot *.aotconf *.class *.jar
-$J17/bin/javac HelloStream.java
-$J17/bin/jar cf hellostream.jar *.class
+run_with() {
+	OPTS=$1
 
-# Go!
+	OPTS="$OPTS -cp hellostream.jar"
+	rm -f *.aot *.aotconf *.class *.jar
+	$J17/bin/javac HelloStream.java
+	$J17/bin/jar cf hellostream.jar *.class
 
-lscpu | grep "Model name"
-echo
+	# Go!
+	lscpu | grep "Model name"
+	echo
 
-echo "JDK 17"
-hyperfine $HF_OPTS "$J17/bin/java $OPTS HelloStream"
+	echo "JDK 17"
+	hyperfine $HF_OPTS "$J17/bin/java $OPTS HelloStream"
 
-echo "JDK 21"
-hyperfine $HF_OPTS "$J21/bin/java $OPTS HelloStream"
+	echo "JDK 21"
+	hyperfine $HF_OPTS "$J21/bin/java $OPTS HelloStream"
 
-echo "JDK MAINLINE"
-hyperfine $HF_OPTS "$JM/bin/java $OPTS HelloStream"
+	echo "JDK 23"
+	hyperfine $HF_OPTS "$J23/bin/java $OPTS HelloStream"
 
-echo "LEYDEN OOB"
-hyperfine $HF_OPTS "$JL/bin/java $OPTS HelloStream"
+	echo "JDK MAINLINE"
+	hyperfine $HF_OPTS "$JM/bin/java $OPTS HelloStream"
 
-# Run AOT
-echo "LEYDEN AOT MISCONFIGURED"
-hyperfine $HF_OPTS "$JL/bin/java -XX:AOTCache=app.aot $OPTS HelloStream"
+	echo "LEYDEN OOTB"
+	hyperfine $HF_OPTS "$JL/bin/java $OPTS HelloStream"
 
-# Build AOT
-$JL/bin/java -XX:AOTMode=record -XX:AOTConfiguration=app.aotconf $OPTS HelloStream
-$JL/bin/java -XX:AOTMode=create -XX:AOTConfiguration=app.aotconf $OPTS -XX:AOTCache=app.aot
+	echo "LEYDEN CDS OFF"
+	hyperfine $HF_OPTS "$JL/bin/java $OPTS -Xshare:off HelloStream"
 
-# Run AOT
-echo "LEYDEN AOT ENABLED"
-hyperfine $HF_OPTS "$JL/bin/java -XX:AOTCache=app.aot $OPTS HelloStream"
+	# Run AOT
+	echo "LEYDEN AOT EMPTY FILE"
+	hyperfine $HF_OPTS "$JL/bin/java -XX:AOTCache=app.aot $OPTS HelloStream"
+
+	# Build AOT
+	$JL/bin/java -XX:AOTMode=record -XX:AOTConfiguration=app.aotconf $OPTS HelloStream
+	$JL/bin/java -XX:AOTMode=create -XX:AOTConfiguration=app.aotconf $OPTS -XX:AOTCache=app.aot
+
+	# Run AOT
+	echo "LEYDEN AOT ENABLED"
+	hyperfine $HF_OPTS "$JL/bin/java -XX:AOTCache=app.aot $OPTS HelloStream"
+}
+
+run_with "-Xmx256m -Xms256m -XX:+UseSerialGC"					| tee results-serial.txt
+run_with "-Xmx256m -Xms256m -XX:+UseParallelGC"					| tee results-parallel.txt
+run_with "-Xmx256m -Xms256m -XX:+UseG1GC"					| tee results-g1.txt
+run_with "-Xmx256m -Xms256m -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC" | tee results-epsilon.txt
+
 
