054fa769a28938239feb427679081e6ae2769e12
Aleksey Shipilev <aleksey@shipilev.net>
2025-04-16 11:26:26 +0200

    Move files around

--- run-jdk-overview.sh
+++ run-jdk-overview.sh
@@ -1,62 +1,15 @@
 #/bin/bash
 
+. setup.sh
+
 # CPU config
 NODES=0-15
-#NODES=0-31
-sudo cpupower frequency-set -g performance
 
 # Config
 HF_OPTS="-w 30 -r 100"
-#HF_OPTS="-w 1 -r 5"
 
-J17=jdk-17
-J21=jdk-21
-J24=jdk-24
-JM=jdk-mainline
-JL=jdk-leyden
-#JL=/home/shade/trunks/shipilev-leyden/build/linux-x86_64-server-release/images/jdk/
-
-# Pull the binaries if not present
-if [ ! -d $J17 ]; then
-  curl https://builds.shipilev.net/openjdk-jdk17-dev/openjdk-jdk17-dev-linux-x86_64-server.tar.xz | tar xJf -
-  mv jdk/ $J17/
-fi
-
-if [ ! -d $J21 ]; then
-  curl https://builds.shipilev.net/openjdk-jdk21-dev/openjdk-jdk21-dev-linux-x86_64-server.tar.xz | tar xJf -
-  mv jdk/ $J21/
-fi
-
-if [ ! -d $J24 ]; then
-  curl https://builds.shipilev.net/openjdk-jdk24/openjdk-jdk24-linux-x86_64-server.tar.xz | tar xJf -
-  mv jdk/ $J24/
-fi
-
-if [ ! -d $JM ]; then
-  curl https://builds.shipilev.net/openjdk-jdk/openjdk-jdk-linux-x86_64-server.tar.xz | tar xJf -
-  mv jdk/ $JM/
-fi
-
-if [ ! -d $JL ]; then
-  curl https://builds.shipilev.net/openjdk-jdk-leyden-premain/openjdk-jdk-leyden-premain-linux-x86_64-server.tar.xz | tar xJf -
-  mv jdk/ $JL/
-fi
-
-# Prepare JAR
-cat > HelloStream.java <<EOF
-import java.util.*;
-import java.util.stream.*;
-
-public class HelloStream {
-    public static void main(String ... args) {
-        var words = List.of("hello", "fuzzy", "world");
-        var greeting = words.stream()
-            .filter(w -> !w.contains("z"))
-            .collect(Collectors.joining(", "));
-        System.out.println(greeting);  // hello, world
-    }
-}
-EOF
+OUT=results/jdk-overview/
+mkdir -p $OUT
 
 run_with() {
 	OPTS=$1
@@ -122,10 +75,10 @@ run_with() {
 	taskset -c $NODES hyperfine $HF_OPTS "$JL/bin/java -XX:AOTCache=app.aot $LEYDEN_OPTS $APP"
 }
 
-run_with "-XX:+UseSerialGC"					| tee results-serial.txt
-run_with "-XX:+UseParallelGC"					| tee results-parallel.txt
-run_with "-XX:+UseG1GC"		 				| tee results-g1.txt
-run_with "-XX:+UseShenandoahGC"		 			| tee results-shenandoah.txt
-run_with "-XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"	| tee results-epsilon.txt
+run_with "-XX:+UseSerialGC"					| tee $OUT/results-serial.txt
+run_with "-XX:+UseParallelGC"					| tee $OUT/results-parallel.txt
+run_with "-XX:+UseG1GC"		 				| tee $OUT/results-g1.txt
+run_with "-XX:+UseShenandoahGC"		 			| tee $OUT/results-shenandoah.txt
+run_with "-XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"	| tee $OUT/results-epsilon.txt
 
 cd ..
